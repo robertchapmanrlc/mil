915fc5db94f10459cdaef3d6d9966917f55ff13a
Robert Chapman <chapmanrobert12@gmail.com>
2023-10-20 02:50:47 -0500

    Changed the README file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <h1 className="text-center text-2xl">Music Tutor Learning</h1>
+    <h1 className="text-center text-2xl">Music Instrument Learning</h1>
   )
 }
