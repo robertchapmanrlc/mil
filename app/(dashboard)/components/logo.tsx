3766cc3b2d6139d95c30aed0d4379005e7602a03
Robert Chapman <chapmanrobert12@gmail.com>
2023-10-20 21:30:26 -0500

    Added logo to side bar

---
+++ app/(dashboard)/components/logo.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Logo() {
+  return <Image height={30} width={30} alt="logo" src="/treble.svg" />;
+}
--- app/(dashboard)/components/side-bar.tsx
+++ app/(dashboard)/components/side-bar.tsx
@@ -1,3 +1,10 @@
+import Logo from "./logo";
+
 export default function SideBar() {
-  return <div className="h-full bg-green-600"></div>;
+  return <div className="h-full flex flex-col items-center bg-green-600 overflow-y-auto shadow-sm border-r">
+    <div className="flex items-center pt-3">
+      <Logo />
+      <h1 className="text-lg text-white">Music Instrument Learning</h1>
+    </div>
+  </div>;
 }
