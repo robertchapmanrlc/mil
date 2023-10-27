8ba453d8caae288b5a062a96fe44bd17034b19ab
Robert Chapman <chapmanrobert12@gmail.com>
2023-10-27 17:04:19 -0500

    Added styling to the create course page using shadcn components

--- app/(dashboard)/(routes)/teacher/courses/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/create/page.tsx
@@ -1,29 +1,44 @@
 "use client";
 
-import { createCourseFormSchema, createCourseFormSchemaType } from "@/lib/types";
-import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
-import { useRouter } from "next/navigation";
-import { useForm, type FieldValues } from "react-hook-form";
 import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, type FieldValues } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import {
+  createCourseFormSchema,
+  createCourseFormSchemaType,
+} from "@/lib/types";
+import Link from "next/link";
 
 export default function CreateCoursePage() {
-
   const router = useRouter();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<createCourseFormSchemaType>({
+  const form = useForm<createCourseFormSchemaType>({
     resolver: zodResolver(createCourseFormSchema),
+    defaultValues: {
+      name: "",
+    },
   });
+
+  const { isSubmitting, isValid } = form.formState;
 
   const onSubmit = async (data: FieldValues) => {
     try {
       const response = await axios.post("/api/courses", data);
-      router.refresh();
-      router.push('/');
+      router.push(`/teacher/courses/${response.data.id}`);
       toast.success("Course Created");
     } catch (error) {
       toast.error("Something went wrong");
@@ -31,27 +46,54 @@ export default function CreateCoursePage() {
   };
 
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col items-start gap-y-2"
-      >
-        <label className="text-2xl">Course Name</label>
-        <input
-          {...register("name")}
-          type="text"
-          className="border w-60"
-          placeholder="...beginner piano"
-        />
-        {errors.name && <p>{errors.name.message}</p>}
-        <button
-          disabled={isSubmitting}
-          type="submit"
-          className="disabled:opacity-40"
-        >
-          Add Course
-        </button>
-      </form>
+    <div className="max-w-5xl h-full flex flex-col md:justify-center md:items-center mx-auto p-6">
+      <div>
+        <h1 className="text-2xl">Add Course Name</h1>
+        <p className="text-sm text-neutral-500">
+          What will you name your course?
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="pt-8 space-y-5"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. Beginner Piano Lessons"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What instrument will your course focus on?
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href={`/teacher/courses`}>
+                <Button type="button" variant="ghost">
+                  Cancel
+                </Button>
+              </Link>
+              <Button
+                type="submit"
+                variant="custom"
+                disabled={isSubmitting || !isValid}
+              >
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 }
