0f7882d0464871d567068009b3bfd3623aebe75e
Robert Chapman <chapmanrobert12@gmail.com>
2023-11-01 04:48:20 -0500

    Courses can now be published and unpublished via api routes

---
+++ app/api/courses/[courseId]/unpublish/route.ts
@@ -0,0 +1,30 @@
+import { database } from "@/lib/database";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const publishedCourse = await database.course.update({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+      data: {
+        isPublished: false,
+      },
+    });
+
+    return NextResponse.json(publishedCourse);
+  } catch (error) {
+    console.log("[COURSE_ID]", error);
+  }
+}
