f93f1eec74defc4a36d450ce0931d42e9ec02b53
Robert Chapman <chapmanrobert12@gmail.com>
2023-11-02 00:29:39 -0500

    Made a data table for the courses teachers have made on their dashboard

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { getTeacherCourses } from "@/actions/get-courses";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { DataTable } from "./components/data-table";
+import { columns } from "./components/columns";
 
 export default async function TeacherDashboard() {
 
@@ -15,15 +17,8 @@ export default async function TeacherDashboard() {
   const courses = await getTeacherCourses(userId);
 
   return (
-    <div className="w-full flex flex-col items-start">
-      <Link href="/teacher/courses/create">
-        <button>Add a course</button>
-      </Link>
-      {courses.map((course) => (
-        <Link key={course.id} href={`/teacher/courses/${course.id}`}>
-          {course.name}
-        </Link>
-      ))}
+    <div className="p-6">
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 }
