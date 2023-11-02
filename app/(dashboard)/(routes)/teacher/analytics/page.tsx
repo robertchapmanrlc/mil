463f8d4e106f5b55fccdb157c2c46860ce416ab1
Robert Chapman <chapmanrobert12@gmail.com>
2023-11-02 01:05:54 -0500

    Added two cards on the teacher analytics page that show the total sales and total revenue for their courses

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,6 +1,7 @@
 import { getAnalytics } from "@/actions/get-analytics";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import DataCard from "./components/data-card";
 
 
 export default async function TeacherAnalytics() {
@@ -12,5 +13,19 @@ export default async function TeacherAnalytics() {
 
   const { data, totalRevenue, totalSales } = await getAnalytics(userId);
 
-  return <div>Teacher Analytics</div>;
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+        <DataCard
+          label="Total Sales"
+          value={totalSales}
+        />
+        <DataCard
+          label="Total Revenue"
+          shouldFormat
+          value={totalRevenue}
+        />
+      </div>
+    </div>
+  );
 }
