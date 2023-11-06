6b94665c184d4589bbbc50ae83d9d341544a9eb5
Robert Chapman <chapmanrobert12@gmail.com>
2023-11-06 14:00:10 -0600

    Removed search input from the navbar

--- app/(dashboard)/components/search-input.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function SearchInput() {
-    return (
-        <div>
-            Search Input
-        </div>
-    );
-}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,4 @@
 import { UserButton } from "@clerk/nextjs";
-import SearchInput from "../app/(dashboard)/components/search-input";
 import TeacherModeButton from "./teacher-mode-button";
 import MobileSidebar from "../app/(dashboard)/components/mobile-sidebar";
 
@@ -7,7 +6,6 @@ export default function Navbar() {
   return (
     <div className="h-full flex items-center bg-white border-b shadow-sm p-4">
       <MobileSidebar />
-      <SearchInput />
       <div className="flex items-center gap-x-5 ml-auto">
         <TeacherModeButton />
         <UserButton afterSignOutUrl="/" />
